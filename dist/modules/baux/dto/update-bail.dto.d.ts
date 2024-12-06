1fbe9b07ce46cbcfd7d2e85d6215989f14e7f846
youssef lachguer <yousseflachguer25@gmail.com>
2024-12-06 12:12:40 +0100

    delete DIST

--- dist/modules/baux/dto/update-bail.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateBailDto } from './create-bail.dto';
-declare const UpdateBailDto_base: import("@nestjs/common").Type<Partial<CreateBailDto>>;
-export declare class UpdateBailDto extends UpdateBailDto_base {
-}
-export {};
--- dist/modules/biens/dto/update-bien.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateBienDto } from './create-bien.dto';
-declare const UpdateBienDto_base: import("@nestjs/common").Type<Partial<CreateBienDto>>;
-export declare class UpdateBienDto extends UpdateBienDto_base {
-}
-export {};
